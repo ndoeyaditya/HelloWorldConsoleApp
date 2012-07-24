c149491d012fbd4ac9dd6565409da90ad8d4254e
Cahyadi Hendrawan <cahyadi.hendrawan@bullseye.com.au>
2012-07-24 13:26:26 +0800

    refactoring 1

--- HelloWorldConsoleApp/Domain/Person.cs.BASE.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HelloWorldConsoleApp.Domain
-{
-    public class Person : IEntity
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
--- Lib/Domain/Person.cs
+++ Lib/Domain/Person.cs
@@ -4,7 +4,7 @@ namespace HelloWorldConsoleApp.Domain
 {
     public class Person : IEntity
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime DateOfBirth { get; set; }
