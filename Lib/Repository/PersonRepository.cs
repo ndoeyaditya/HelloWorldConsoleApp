d67aec230a18b14f300d4ce5063485b54d6e156a
Cahyadi Hendrawan <cahyadi.hendrawan@bullseye.com.au>
2012-07-24 13:29:15 +0800

    fix build

--- Lib/Repository/PersonRepository.cs
+++ Lib/Repository/PersonRepository.cs
@@ -12,11 +12,12 @@ namespace HelloWorldConsoleApp.Repository
             var persons = new List<Person>();
             for (int i = 0; i < 50; i++)
             {
+                var id = Guid.NewGuid();
                 persons.Add(
                     new Person()
                     {
-                        Id = i + 1,
-                        Name = string.Format("Person {0}", (i + 1).ToString(CultureInfo.InvariantCulture)),
+                        Id = id,
+                        Name = string.Format("Person {0}", id.ToString()),
                         CreatedDate = DateTime.Now
                     });
             }
